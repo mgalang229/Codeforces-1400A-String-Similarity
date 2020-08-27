3294bb0ab7896ca252de179a1fb38dad38635373
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-08-27 16:02:36 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -2,25 +2,20 @@
 
 using namespace std;
 
-void test_case() {
-	int n;
-	cin >> n;
-	string s;
-	cin >> s;
-	for(int i = 0; i < (int) s.size(); i += 2) {
-		cout << s[i];	
-	}
-	cout << "\n";
-}
-
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	int T;
-	cin >> T;
-	for (int nr = 1; nr <= T; ++nr) {
-		test_case();
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int n;
+		cin >> n;
+		string s;
+		cin >> s;
+		for (int i = 0; i < (int) s.size(); i += 2) {
+			cout << s[i];
+		}
+		cout << '\n';
 	}
-	//test_case();
 	return 0;
 }
